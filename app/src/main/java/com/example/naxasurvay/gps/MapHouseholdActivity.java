5fd3da54fb84368c6f349a28d6d5ff3a47308f47
Samir Dangal <Samir Dangal>
2017-08-15 07:54:19 +0545

    geojson marker added successfully

--- app/src/main/java/com/example/naxasurvay/gps/MapHouseholdActivity.java
+++ app/src/main/java/com/example/naxasurvay/gps/MapHouseholdActivity.java
@@ -1,114 +1,80 @@
 package com.example.naxasurvay.gps;
 
-import android.app.Activity;
-import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 
 import com.example.naxasurvay.R;
 import com.mapbox.mapboxsdk.Mapbox;
-import com.mapbox.mapboxsdk.annotations.PolylineOptions;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
-import com.mapbox.mapboxsdk.style.functions.Function;
-import com.mapbox.mapboxsdk.style.functions.stops.IdentityStops;
-import com.mapbox.mapboxsdk.style.functions.stops.Stops;
-import com.mapbox.mapboxsdk.style.layers.FillExtrusionLayer;
-import com.mapbox.mapboxsdk.style.layers.FillLayer;
-import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
-import com.mapbox.services.commons.utils.TextUtils;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.fillColor;
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.fillExtrusionBase;
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.fillExtrusionColor;
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.fillExtrusionHeight;
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.fillExtrusionOpacity;
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.fillOpacity;
-
 public class MapHouseholdActivity extends AppCompatActivity {
+    private static final String TAG = "MarkerFollowingRoute";
 
-    private static final String TAG = "MapHousehold";
     private MapView mapView;
-    private MapboxMap mapboxMap;
     private MapboxMap map;
+    private Handler handler;
+    private Runnable runnable;
 
-
+    private static int count = 0;
+    private long distance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-// Mapbox access token is configured here. This needs to be called either in your application
+
+        // Mapbox access token is configured here. This needs to be called either in your application
         // object or in the same activity which contains the mapview.
         Mapbox.getInstance(this, getString(R.string.access_token));
 
-        // This contains the MapView in XML and needs to be called after the access token is configured.
+        // This contains the MapView in XML and needs to be called after the account manager
         setContentView(R.layout.activity_map_household);
 
+        // Initialize the map view
         mapView = (MapView) findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
-//        mapView.getMapAsync(new OnMapReadyCallback() {
-//            @Override
-//            public void onMapReady(MapboxMap mapboxMap) {
-//
-//                // Customize map with markers, polylines, etc.
-//                // Load and Draw the GeoJSON
-//                new DrawGeoJson1().execute();
-//
-////                 Add the marathon route source to the map
-////                GeoJsonSource courseRouteGeoJson = new GeoJsonSource("coursedata", loadJsonFromAsset("all_household_data.geojson"));
-////                Log.e(TAG, "onMapReady: "+ courseRouteGeoJson.toString() );
-////                mapboxMap.addSource(courseRouteGeoJson);
-////                addExtrusionsLayerToMap();
-//
-//
-//            }
-//        });
-
         mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
-            public void onMapReady(final MapboxMap mapboxMap) {
+            public void onMapReady(MapboxMap mapboxMap) {
                 map = mapboxMap;
 
-                GeoJsonSource roomDataSource = new GeoJsonSource("room-data", loadJsonFromAsset1("all_household_data.geojson"));
-                map.addSource(roomDataSource);
+                // Load and Draw the GeoJSON. The marker animation is also handled here.
+                new MapHouseholdActivity.DrawGeoJson().execute();
 
-                FillExtrusionLayer roomExtrusionLayer = new FillExtrusionLayer("room-extrusion", "room-data");
-                roomExtrusionLayer.setProperties(
-                        fillExtrusionColor(Function.property("color", new IdentityStops<String>())),
-                        fillExtrusionHeight(Function.property("height", new IdentityStops<Float>())),
-                        fillExtrusionBase(Function.property("base_height", new IdentityStops<Float>())),
-                        fillExtrusionOpacity(0.5f)
-                );
+//                updateMap(45.52214,-122.63748);
 
-                map.addLayer(roomExtrusionLayer);
+
             }
         });
+    } // End onCreate
 
-
-    }
-
-    // Add the mapView lifecycle to the activity's lifecycle methods
     @Override
     public void onResume() {
         super.onResume();
         mapView.onResume();
+        // When the activity is resumed we restart the marker animating.
+        if (handler != null && runnable != null) {
+            handler.post(runnable);
+        }
     }
 
     @Override
@@ -127,6 +93,11 @@ public class MapHouseholdActivity extends AppCompatActivity {
     public void onPause() {
         super.onPause();
         mapView.onPause();
+        // Check if the marker is currently animating and if so, we pause the animation so we aren't
+        // using resources when the activities not in view.
+        if (handler != null && runnable != null) {
+            handler.removeCallbacks(runnable);
+        }
     }
 
     @Override
@@ -147,7 +118,9 @@ public class MapHouseholdActivity extends AppCompatActivity {
         mapView.onSaveInstanceState(outState);
     }
 
-
+    // We want to load in the GeoJSON file asynchronous so the UI thread isn't handling the file
+// loading. The GeoJSON file we are using is stored in the assets folder, you could also get
+// this information from the Mapbox map matching API during runtime.
     private class DrawGeoJson extends AsyncTask<Void, Void, List<LatLng>> {
         @Override
         protected List<LatLng> doInBackground(Void... voids) {
@@ -156,7 +129,8 @@ public class MapHouseholdActivity extends AppCompatActivity {
 
             try {
                 // Load GeoJSON file
-                InputStream inputStream = getAssets().open("assets/all_household_data.geojson");
+                InputStream inputStream = getAssets().open("all_household_data.geojson");
+                Log.e(TAG, "doInBackground: "+inputStream.toString() );
                 BufferedReader rd = new BufferedReader(new InputStreamReader(inputStream, Charset.forName("UTF-8")));
                 StringBuilder sb = new StringBuilder();
                 int cp;
@@ -169,119 +143,33 @@ public class MapHouseholdActivity extends AppCompatActivity {
                 // Parse JSON
                 JSONObject json = new JSONObject(sb.toString());
                 JSONArray features = json.getJSONArray("features");
+
                 JSONObject feature = features.getJSONObject(0);
-                JSONObject geometry = feature.getJSONObject("geometry");
-                if (geometry != null) {
-                    String type = geometry.getString("type");
+                Log.e(TAG, "doInBackground: feature : "+feature.length() );
+                Log.e(TAG, "doInBackground: features : "+features.length() );
+//                JSONObject properties = feature.getJSONObject("properties");
+                for (int i = 0; i < features.length(); i++ ) {
+                    JSONObject jobj = features.getJSONObject(i);
+                    JSONObject geometry = jobj.getJSONObject("geometry");
+                    Log.e(TAG, "doInBackground: geometry"+geometry.toString() );
+                    if (geometry != null) {
+                        String type = geometry.getString("type");
 
-                    // Our GeoJSON only has one feature: a line string
-                    if (!TextUtils.isEmpty(type) && type.equalsIgnoreCase("LineString")) {
+                        // Our GeoJSON only has one feature: a line string
+                        if (!TextUtils.isEmpty(type) && type.equalsIgnoreCase("Point")) {
 
-                        // Get the Coordinates
-                        JSONArray coords = geometry.getJSONArray("coordinates");
-                        for (int lc = 0; lc < coords.length(); lc++) {
-                            JSONArray coord = coords.getJSONArray(lc);
-                            LatLng latLng = new LatLng(coord.getDouble(1), coord.getDouble(0));
-                            points.add(latLng);
+                            // Get the Coordinates
+                            JSONArray coords = geometry.getJSONArray("coordinates");
+//                            for (int lc = 0; lc < coords.length(); lc++) {
+//                                JSONArray coord = coords.getJSONArray(lc);
+                                LatLng latLng = new LatLng(coords.getDouble(1), coords.getDouble(0));
+                                points.add(latLng);
+
+//                            }
                         }
                     }
                 }
-            } catch (Exception exception) {
-                Log.e(TAG, "Exception Loading GeoJSON: " + exception.toString());
-            }
 
-            return points;
-        }
-
-        @Override
-        protected void onPostExecute(List<LatLng> points) {
-            super.onPostExecute(points);
-
-            if (points.size() > 0) {
-
-                // Draw polyline on map
-                mapboxMap.addPolyline(new PolylineOptions()
-                        .addAll(points)
-                        .color(Color.parseColor("#3bb2d0"))
-                        .width(2));
-            }
-        }
-    }
-
-
-
-    private void addExtrusionsLayerToMap() {
-        // Add FillExtrusion layer to map using GeoJSON data
-        FillExtrusionLayer courseExtrusionLayer = new FillExtrusionLayer("course", "coursedata");
-        courseExtrusionLayer.setProperties(
-                fillExtrusionColor(Color.YELLOW),
-                fillExtrusionOpacity(0.7f),
-                fillExtrusionHeight(Function.property("e", Stops.<Float>identity()))
-        );
-        mapboxMap.addLayer(courseExtrusionLayer);
-    }
-
-    private String loadJsonFromAsset(String filename) {
-        // Using this method to load in GeoJSON files from the assets folder.
-        try {
-            InputStream is = getAssets().open(filename);
-            int size = is.available();
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-            return new String(buffer, "UTF-8");
-
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            return null;
-        }
-    }
-
-    /**
-     * @Samir GeoJson Data parsing
-     */
-    // We want to load in the GeoJSON file asynchronous so the UI thread isn't handling the file
-    // loading. The GeoJSON file we are using is stored in the assets folder, you could also get
-    // this information siteName the Mapbox map matching API during runtime.
-    private class DrawGeoJson1 extends AsyncTask<Void, Void, List<LatLng>> {
-        @Override
-        protected List<LatLng> doInBackground(Void... voids) {
-
-            ArrayList<LatLng> points = new ArrayList<>();
-
-            try {
-                // Load GeoJSON file
-                InputStream inputStream = getAssets().open("sudur.geojson");
-                BufferedReader rd = new BufferedReader(new InputStreamReader(inputStream, Charset.forName("UTF-8")));
-                StringBuilder sb = new StringBuilder();
-                int cp;
-                while ((cp = rd.read()) != -1) {
-                    sb.append((char) cp);
-                }
-
-                inputStream.close();
-
-                // Parse JSON
-                JSONObject json = new JSONObject(sb.toString());
-                JSONArray features = json.getJSONArray("features");
-                JSONObject feature = features.getJSONObject(0);
-                JSONObject geometry = feature.getJSONObject("geometry");
-                if (geometry != null) {
-                    String type = geometry.getString("type");
-
-                    // Our GeoJSON only has one feature: a line string
-                    if (!TextUtils.isEmpty(type) && type.equalsIgnoreCase("LineString")) {
-
-                        // Get the mySiteLatLngList
-                        JSONArray coords = geometry.getJSONArray("mySiteLatLngList");
-                        for (int lc = 0; lc < coords.length(); lc++) {
-                            JSONArray coord = coords.getJSONArray(lc);
-                            LatLng latLng = new LatLng(coord.getDouble(1), coord.getDouble(0));
-                            points.add(latLng);
-
-                        }
-                    }
-                }
             } catch (Exception exception) {
                 Log.e(TAG, "Exception Loading GeoJSON: " + exception.toString());
             }
@@ -302,31 +190,25 @@ public class MapHouseholdActivity extends AppCompatActivity {
 //                        .color(Color.parseColor("#d0423b"))
 //                        .width(2));
 
-                for (int i = 0; i < points.size(); i++) {
+                for (int i = 0; i<points.size(); i++){
 
-//                    updateMap(points.get(i).getLatitude(), points.get(i).getLongitude());
+                    updateMap(points.get(i).getLatitude(),points.get(i).getLongitude(), i);
                 }
             }
         }
     }
 
+    private void updateMap(double latitude, double longitude, int i) {
+        // Build marker
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(latitude, longitude))
+                .title("Household No."+i));
 
-
-    private String loadJsonFromAsset1(String filename) {
-        // Using this method to load in GeoJSON files from the assets folder.
-
-        try {
-            InputStream is = getAssets().open(filename);
-            int size = is.available();
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-            return new String(buffer, "UTF-8");
-
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            return null;
-        }
+        // Animate camera to geocoder result location
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(latitude, longitude))
+                .zoom(15)
+                .build();
+        map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition), 5000, null);
     }
-
 }
